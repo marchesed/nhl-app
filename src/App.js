7737e216d8878544efcb0876fe8cdd9667ebe1e3
marchesed <marchesedb@gmail.com>
2019-10-20 22:56:12 -0400

    add standings component and react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NhlMain from './components/NhlMain'
 
 function App() {
   return (
+    
     <div className="App">
       <NhlMain />
     </div>
