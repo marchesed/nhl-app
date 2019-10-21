f3e6ecfc207f001b4f72b35600442874ca1c5155
marchesed <marchesedb@gmail.com>
2019-10-20 22:33:54 -0400

    add css/fixed positioning

--- src/components/Games.js
+++ src/components/Games.js
@@ -16,8 +16,11 @@ const Games = ({games,logoData}) => {
         var homeRecord = game.teams.home.leagueRecord.wins + "-" + game.teams.home.leagueRecord.losses + "-" + game.teams.home.leagueRecord.ot
         var awayRecord = game.teams.away.leagueRecord.wins + "-" + game.teams.away.leagueRecord.losses + "-" + game.teams.away.leagueRecord.ot
         return game.status.abstractGameState === "Live" ||  game.status.abstractGameState === "Final" ? (
-            <div key={game.gamePk} className="game-box col-md-6 row">
+            <div key={game.gamePk} className="game-box col-md-5 row">
                 <div className="col-8">
+                    {game.status.abstractGameState === "Live" &&
+                        <p className="live-text">LIVE</p>
+                    }
                     <p className="game-text"><span className="sprite" style={{backgroundPositionX: positionXAway,backgroundPositionY: positionYAway }}></span><span>{game.teams.away.team.name}</span><br></br>({awayRecord})</p>
                     <p className="at-text">@</p>
                     <p className="game-text"><span className="sprite" style={{backgroundPositionX: positionXHome,backgroundPositionY: positionYHome }}></span><span>{game.teams.home.team.name}</span><br></br>({homeRecord})</p>
@@ -28,7 +31,7 @@ const Games = ({games,logoData}) => {
                     <p className="time-text pt-50"><strong>{game.linescore.teams.home.goals}</strong></p>
                 </div>
             </div>
-        ) : <div key={game.gamePk} className="game-box col-md-6 row">
+        ) : <div key={game.gamePk} className="game-box col-md-5 row">
         <div className="col-8">
             <p className="game-text"><span className="sprite" style={{backgroundPositionX: positionXAway,backgroundPositionY: positionYAway }}></span><span>{game.teams.away.team.name}</span><br></br>({awayRecord})</p>
             <p className="at-text">@</p>
